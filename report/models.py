1d55b97c8fc85112da2251f5107559eff5db3db2
spectrum705 <shubhamjha93@gmail.com>
2023-01-25 00:27:13 +0530

    Integrating attendance module

--- report/models.py
+++ report/models.py
@@ -9,14 +9,19 @@ def load_user(user_id):
     #this is our database model of a user, made more changes will upload later
 
 
-class Teachers(db.DynamicDocument, UserMixin):
+
+class Account(db.DynamicDocument, UserMixin):
     _id=db.IntField(primary_key=True)
     name=db.StringField(max_length=20, required=True)
     # password = db.StringField( required=True)
     classes=db.ListField(db.DictField())
     isAdmin=db.BooleanField()
     isStudent=db.BooleanField()
-        
+    accountType=db.StringField()    
+
+
+
+class Teachers(Account):      
   
     def to_json(self):
         return {
@@ -26,7 +31,6 @@ class Teachers(db.DynamicDocument, UserMixin):
             "classes": self.classes
         }
     
-   
 
 class Marks(db.DynamicEmbeddedDocument):
     pass
@@ -36,15 +40,50 @@ class Marks(db.DynamicEmbeddedDocument):
     # test_4=db.DictField()
 
 class Students(db.Document):
-    _id=db.IntField(primary_key=True) #enter admission no here
-    name=db.StringField( required=True)
     standard = db.StringField(required=True)
     marks=db.EmbeddedDocumentField(Marks)
     isGraded=db.BooleanField
-    # roll_number=db.IntField(required=True)
-    
+
+
 class Attendance(db.DynamicDocument):
     _id=db.IntField(primary_key=True)
     name=db.StringField(max_length=20, required=True)
     school=db.StringField(max_length=20, required=True)
     semester=db.IntField(required=True)
+
+
+
+# class Teachers(db.DynamicDocument, UserMixin):
+#     _id=db.IntField(primary_key=True)
+#     name=db.StringField(max_length=20, required=True)
+#     # password = db.StringField( required=True)
+#     classes=db.ListField(db.DictField())
+#     isAdmin=db.BooleanField()
+#     isStudent=db.BooleanField()
+        
+  
+#     def to_json(self):
+#         return {
+#             "name":self.name,
+#             "password":self.password,
+#             "id":self._id,
+#             "classes": self.classes
+#         }
+    
+   
+
+# class Marks(db.DynamicEmbeddedDocument):
+#     pass
+#     # test_1=db.DictField()
+#     # test_2=db.DictField()
+#     # test_3=db.DictField()
+#     # test_4=db.DictField()
+
+# class Students(db.Document):
+#     _id=db.IntField(primary_key=True) #enter admission no here
+#     name=db.StringField( required=True)
+#     standard = db.StringField(required=True)
+#     marks=db.EmbeddedDocumentField(Marks)
+#     isGraded=db.BooleanField
+    # roll_number=db.IntField(required=True)
+    
